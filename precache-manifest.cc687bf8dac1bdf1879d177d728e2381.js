f1ed3ceb64a063f7881f479ac747bf8cc646eaac
dazza-debug <sevotastic.st@gmail.com>
2021-10-01 16:06:17 +1000

    Updates

---
+++ precache-manifest.cc687bf8dac1bdf1879d177d728e2381.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf419d6c0db23015166a8fa18b727848",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c13718858c67354efed",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "a84f4f945fc9c92b7f0d",
+    "url": "/static/css/main.9a8415d2.chunk.css"
+  },
+  {
+    "revision": "4c13718858c67354efed",
+    "url": "/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84f4f945fc9c92b7f0d",
+    "url": "/static/js/main.21b5f143.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
